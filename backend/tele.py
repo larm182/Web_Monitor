9671c9f6cd945b00542ba6e8e82edb8a2fd8d56d
Luis Ángel Ramirez Mendoza <larm182@gmail.com>
2025-06-19 11:27:10 -0500

    Update tele.py

--- backend/tele.py
+++ backend/tele.py
@@ -2,10 +2,10 @@ import asyncio
 from telegram import Bot
 
 # Reemplaza 'YOUR_BOT_TOKEN' con el token que obtuviste de BotFather
-TOKEN = '5562872839:AAGD06yp79PMg9OGy27_fgkVnlG7zKqM42s'
+TOKEN = 'YOUR_BOT_TOKEN'
 
 # Reemplaza 'YOUR_CHAT_ID' con el ID de tu chat (o grupo) donde quieres enviar el mensaje
-CHAT_ID = '779516132'
+CHAT_ID = 'YOUR_CHAT_ID'
 
 async def main():
     bot = Bot(token=TOKEN)
